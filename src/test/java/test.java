cd5a360d129a2ac9a4e5005b141384bd87e3d7c0
Eslam Mashaly <eslam.mashaly@vodafone.com.eg>
2023-08-28 15:38:08 +0300

    BaseTest class

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -9,24 +9,13 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class test {
+public class test extends BaseTest{
     @Test
     public void AppiumTest() throws MalformedURLException {
-        AppiumDriverLocalService service=new AppiumServiceBuilder()
-                .withAppiumJS(new File("C://Users//TECHNICAL//AppData//Roaming//npm//node_modules//appium//build//lib//main.js"))
-                .withIPAddress("127.0.0.1")
-                .usingPort(4723)
-                .build();
-        service.start();
 
-        UiAutomator2Options options=new UiAutomator2Options();
-        options.setDeviceName("S23 Ultra");
-        options.setApp("E:\\AppiumDemo\\src\\main\\resources\\ApiDemos-debug.apk");
 
-        AndroidDriver driver=new AndroidDriver(new URL("http://127.0.0.1:4723"),options);
         driver.findElement(AppiumBy.accessibilityId("Preference")).click();
-        driver.quit();
-        service.stop();
+
 
 
 
